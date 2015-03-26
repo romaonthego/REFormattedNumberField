0ce837020109dfe1807f8aa279a09c67f6dd14a3
Aleksandr Dodatko <a.dodatko@healthjoy.com>
2015-03-26 13:38:29 +0200

    Added framework targets

---
+++ REFormattedNumberField-iOS-dynamic/REFormattedNumberField-iOS-dynamic.h
@@ -0,0 +1,19 @@
+//
+//  REFormattedNumberField-iOS-dynamic.h
+//  REFormattedNumberField-iOS-dynamic
+//
+//  Created by Oleksandr Dodatko on 3/26/15.
+//  Copyright (c) 2015 healthjoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for REFormattedNumberField-iOS-dynamic.
+FOUNDATION_EXPORT double REFormattedNumberField_iOS_dynamicVersionNumber;
+
+//! Project version string for REFormattedNumberField-iOS-dynamic.
+FOUNDATION_EXPORT const unsigned char REFormattedNumberField_iOS_dynamicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <REFormattedNumberField_iOS_dynamic/PublicHeader.h>
+
+
